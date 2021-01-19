f55214add3fd59a0159b639aefbd8961da01b10b
vagrant <vagrant@localhost.localdomain>
2021-01-19 09:39:53 +0000

    Add user_login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password]) #ユーザーがデータベースにあり、かつ、認証に成功した場合にのみture
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+end
