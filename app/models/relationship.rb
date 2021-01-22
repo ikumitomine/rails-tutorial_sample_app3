1c4a0dc631760d01b08eff402bacaf4da62d0829
vagrant <fki.193@icloud.com>
2021-01-22 09:24:50 +0000

    Add user following

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,6 @@
 class Relationship < ApplicationRecord
+  belongs_to :follower, class_name: "User" # Userクラスを指定し、followeという名前をbelongs_toする
+  belongs_to :followed, class_name: "User"
+  validates :follower_id, presence: true
+  validates :followed_id, presence: true
 end
--- db/migrate/20210121092619_create_relationships.rb
+++ db/migrate/20210121092619_create_relationships.rb
@@ -7,7 +7,6 @@ class CreateRelationships < ActiveRecord::Migration[5.2]
       t.timestamps
     end
 
-  end
   add_index :relationships, :follower_id
   add_index :relationships, :followed_id
   add_index :relationships, [:follower_id, :followed_id], unique: true
